d16943c3a6a623c401385e540bf7b83424d13f4e
Alexander Sapozhnikov <shoorick@cpan.org>
2021-01-04 03:13:10 +0500

    Preserve existing Cyrillic glyphs during composing

--- sources/composite-cyrillic.sh
+++ sources/composite-cyrillic.sh
@@ -2,12 +2,12 @@
 
 for font in $(ls -d *.ufo);
 do
-    psfbuildcomp -i ../composite/cyrillic-common.txt $font
+    psfbuildcomp -i ../composite/cyrillic-common.txt --preserve cyrillic $font
 
     if [[ $font == *"Italic"* ]]
     then
-        psfbuildcomp -i ../composite/cyrillic-italic.txt $font
+        psfbuildcomp -i ../composite/cyrillic-italic.txt --preserve cyrillic $font
     else
-        psfbuildcomp -i ../composite/cyrillic-straight.txt $font
+        psfbuildcomp -i ../composite/cyrillic-straight.txt --preserve cyrillic $font
     fi
 done
