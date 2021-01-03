6df945cedd882eb3953e7fff15ef9f9f9fcfccc4
Alexander Sapozhnikov <shoorick@cpan.org>
2021-01-03 05:52:45 +0500

    Apply different rules for italic and straight fonts

--- sources/composite-cyrillic.sh
+++ sources/composite-cyrillic.sh
@@ -1,4 +1,11 @@
 for font in $(ls -d *.ufo);
 do
-    psfbuildcomp -i ../composite/cyrillic.txt --preserve cyrillic $font
+    psfbuildcomp -i ../composite/cyrillic-common.txt $font
+
+    if [[ $font == *"Italic"* ]]
+    then
+        psfbuildcomp -i ../composite/cyrillic-italic.txt $font
+    else
+        psfbuildcomp -i ../composite/cyrillic-straight.txt $font
+    fi
 done
